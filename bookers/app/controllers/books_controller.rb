6461ad8ef59088fc72ebab5664347858b6f5869c
ybnr1130 <muuckk@gmail.com>
2021-08-24 10:31:57 +0000

    [Update]

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -2,6 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def index
@@ -43,7 +45,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- bookers/app/controllers/users_controller.rb
+++ bookers/app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @newbook = Book.new
   end
 
   def index
     @users = User.all
-    @book = Book.new
+    @newbook = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
